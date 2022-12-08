7da703e72d0da1c6296b7822fe611688eb2b79b0
Rodolfo Lima <rodolfo-m16@hotmail.com>
2022-12-07 23:20:46 -0300

    adding projects

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -5,7 +5,7 @@ function Card({ name, img, type }) {
   return (
     <div className={type ? styles.has_proj : styles.skill_card}>
       <h3>{name}</h3>
-      <hr />
+
       <div className={styles.img_box}>
         <img className={styles.skill_img} src={img} alt={name} />
       </div>
--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,11 +1,25 @@
 import Card from "../layout/Card";
 import styles from "./Projetos.module.css";
 import ilus from "../img/Ilustração.svg";
+// import { motion } from "framer-motion";
+// import { useState, useEffect, useRef } from "react";
 
 // importando projetos
 import imgCosts from "../img/projects/costs.png";
+import imgSimon from "../img/projects/simonGame.png";
+
+// import imgCss from "../img/card-imgs/CSS.png";
+
+// const projects = [imgCosts, imgSimon, imgCss, imgCosts, imgSimon, imgCss];
 
 function Projetos() {
+  // const carousel = useRef();
+  // const [width, setWidth] = useState(0);
+
+  // useEffect(() => {
+  //   setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
+  // }, []);
+
   return (
     <section className={styles.section}>
       <div className={styles.title}>
@@ -16,11 +30,50 @@ function Projetos() {
         <a href="https://resonant-pudding-e4f9c9.netlify.app/">
           <Card name="Costs" img={imgCosts} type="proj" />
         </a>
-        <Card name="Em breve" />
-        <Card name="Em breve" />
+        <a href="https://nimble-biscotti-5ce51e.netlify.app/">
+          <Card name="Simon Game" img={imgSimon} type="proj" />
+        </a>
         <Card name="Em breve" />
         <Card name="Em breve" />
       </div>
+      {/* <div className={styles.carousel_section}>
+        <motion.div
+          ref={carousel}
+          className={styles.carousel}
+          whileTap={{ cursor: "grabbing" }}
+        >
+          <motion.div
+            className={styles.inner}
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 1.0 }}
+          >
+            <motion.div className={styles.item}>
+              <a href="https://resonant-pudding-e4f9c9.netlify.app/">
+                <Card name="Costs" img={imgCosts} type="proj" />
+              </a>
+            </motion.div>
+            <motion.div className={styles.item}>
+              <a href="https://nimble-biscotti-5ce51e.netlify.app/">
+                <Card name="Simon Game" img={imgSimon} type="proj" />
+              </a>
+            </motion.div>
+            <motion.div className={styles.item}>
+              <Card name="Em breve" />
+            </motion.div>
+            <motion.div className={styles.item}>
+              <Card name="Em breve" />
+            </motion.div>
+            {/**************** {projects.map((proj) => (
+              <motion.div className={styles.item} key={proj}>
+                <img src={proj} alt={"Text alt"} />
+              </motion.div>
+            ))} }
+          </motion.div>
+        </motion.div>
+      </div> */}
     </section>
   );
 }
